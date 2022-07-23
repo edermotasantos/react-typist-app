3eee711e8014464b1adc6d8f299fb52725130a52
Eder Santos <3derms@gmail.com>
2022-07-23 14:26:50 -0300

    style: ajusta header

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 
 function Header () {
   return (
-    <h3>React Typist App</h3>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ background: '#2E3B55' }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React Typist App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
